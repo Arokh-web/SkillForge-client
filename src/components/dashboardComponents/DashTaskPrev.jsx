b9daa4f8b681a8e6a876c0905cf7202511263f79
Arokh-web <michaelabudich@gmail.com>
2025-05-28 19:22:11 +0200

    minor correction

--- src/components/dashboardComponents/DashTaskPrev.jsx
+++ src/components/dashboardComponents/DashTaskPrev.jsx
@@ -1,10 +1,9 @@
-import { TaskContext, ProjectContext } from "../../contexts/contexts";
+import { TaskContext } from "../../contexts/contexts";
 import React, { useContext } from "react";
 import fetchData from "../../API/fetchData";
 
 const DashTaskPrev = () => {
   const { selectedTask, setSelectedTask } = useContext(TaskContext);
-  const { selectedProject } = useContext(ProjectContext);
 
   console.log("Selected Tasks:", selectedTask);
 
@@ -25,89 +24,95 @@ const DashTaskPrev = () => {
   };
 
   return (
-    <>
-      <div className="dash-task-prev">
-        <p className="sub-title ">Overview Selected Tasks:</p>
+    <div className="dash-task-prev">
+      <p className="sub-title ">Overview Selected Tasks:</p>
+      <table className="task-table">
+        <thead>
+          <tr>
+            <th>Task</th>
+            <th>Description</th>
+            <th>To be done until</th>
+          </tr>
+        </thead>
 
-        {selectedTask.length === 0 ? (
-          <div className="dash-task-item-list">
-            <div>No tasks selected</div>
-          </div>
-        ) : (
-          <table className="">
-            <thead>
-              <tr>
-                <th>Task</th>
-                <th>Description</th>
-                <th>To be done until</th>
-              </tr>
-            </thead>
-            <tbody>
-              {selectedTask.map((task) => [
-                <React.Fragment key={task.id}>
-                  <tr key={`${task.id}-row}`} className="task-item">
-                    <td>
-                      <p className="text-sm font-medium">{task.title}</p>
-                    </td>
-                    <td>
-                      <p className="text-xs">{task.content}</p>
-                    </td>
-                    <td>
-                      <p className="text-xs">{task.deadline}</p>
-                    </td>
-                  </tr>
+        <tbody>
+          {selectedTask.length === 0 ? (
+            <tr>
+              <td colSpan="3">
+                <div className="dash-task-item-list">
+                  <div>No tasks selected</div>
+                </div>
+              </td>
+            </tr>
+          ) : (
+            selectedTask.map((task) => (
+              <React.Fragment key={task.id}>
+                <tr key={`${task.id}-row}`} className="task-item">
+                  <td colSpan="1">
+                    <p className="text-sm font-medium">{task.title}</p>
+                  </td>
+                  <td></td>
+                  <td colSpan="1">
+                    <p className="text-xs">{task.deadline}</p>
+                  </td>
+                </tr>
+                <tr>
+                  <td colSpan="2">
+                    <p className="text-xs">{task.content}</p>
+                  </td>
+                  <td></td>
+                </tr>
 
-                  <tr key={`${task.id}-radio}`} className="task-radiobuttons">
-                    <td>
-                      Done?
-                      <input
-                        type="radio"
-                        name={`radio-${task.id}`}
-                        defaultChecked={task.status === "done"}
-                        className="checkbox"
-                        value="done"
-                        onChange={(e) =>
-                          handleCheckboxChange(task, e.target.value)
-                        }
-                      />
-                    </td>
-                    <td>
-                      Active?
-                      <input
-                        type="radio"
-                        name={`radio-${task.id}`}
-                        defaultChecked={task.status === "active"}
-                        className="checkbox"
-                        value="active"
-                        onChange={(e) =>
-                          handleCheckboxChange(task, e.target.value)
-                        }
-                      />
-                    </td>
-                    <td>
-                      Planned?
-                      <input
-                        type="radio"
-                        name={`radio-${task.id}`}
-                        defaultChecked={task.status === "planned"}
-                        className="checkbox"
-                        value="planned"
-                        onChange={(e) =>
-                          handleCheckboxChange(task, e.target.value)
-                        }
-                      />
-                    </td>
-                  </tr>
-                  {/* <button name="open-notes btn-primary">
+                <tr key={`${task.id}-radio}`} className="task-radiobuttons">
+                  <td>
+                    Done?
+                    <input
+                      type="radio"
+                      name={`radio-${task.id}`}
+                      defaultChecked={task.status === "done"}
+                      className="checkbox"
+                      value="done"
+                      onChange={(e) =>
+                        handleCheckboxChange(task, e.target.value)
+                      }
+                    />
+                  </td>
+                  <td>
+                    Active?
+                    <input
+                      type="radio"
+                      name={`radio-${task.id}`}
+                      defaultChecked={task.status === "active"}
+                      className="checkbox"
+                      value="active"
+                      onChange={(e) =>
+                        handleCheckboxChange(task, e.target.value)
+                      }
+                    />
+                  </td>
+                  <td>
+                    Planned?
+                    <input
+                      type="radio"
+                      name={`radio-${task.id}`}
+                      defaultChecked={task.status === "planned"}
+                      className="checkbox"
+                      value="planned"
+                      onChange={(e) =>
+                        handleCheckboxChange(task, e.target.value)
+                      }
+                    />
+                  </td>
+                </tr>
+                {/* <button name="open-notes btn-primary">
                       Open connetected notes
                     </button> */}
-                </React.Fragment>,
-              ])}
-            </tbody>
-          </table>
-        )}
-      </div>
-    </>
+              </React.Fragment>
+            ))
+          )}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
