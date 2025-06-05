b9ea5ff259aeb208342d8c5c71b4eb72a13ecd7b
Arokh-web <michaelabudich@gmail.com>
2025-06-05 09:22:29 +0200

    changed styles to fit page and content. working task list done

--- src/components/heroComponents/Hero.jsx
+++ src/components/heroComponents/Hero.jsx
@@ -5,7 +5,7 @@ import { Outlet } from "react-router-dom";
 
 const Hero = () => {
   return (
-    <div className="hero">
+    <div className="hero hero-fade-mask">
       <div>
         <Outlet />
       </div>
