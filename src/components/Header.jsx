f5ee5e285f99b733ab665171aa854880a5f83f53
Arokh-web <michaelabudich@gmail.com>
2025-06-06 18:52:39 +0200

    working signout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,21 @@
 // This is the upper menu bar, where the logo, the title of the current page, dark-mode-switch, logout-button and account-button exist
 import { Link } from "react-router-dom";
 import { useAuthContext } from "../contexts/contexts";
+import { useNavigate } from "react-router-dom";
+import fetchData from "../API/fetchData";
 
 const Header = () => {
   // const [darkMode, setDarkMode] = useState(false);
-  const { user } = useAuthContext();
+  const { user, setUser } = useAuthContext();
+  const navigate = useNavigate();
+
+  const handleClickLogout = async () => {
+    
+    await fetchData("POST", "/auth/signout", );
+    // Clear user state and navigate to signinup page
+    setUser(null);
+    navigate("signinup");
+  };
 
   return (
     // HEADER: with logo-container and header-content
@@ -29,7 +40,9 @@ const Header = () => {
             </div>
             <div className="dark-mode-button"></div>
             <div className="logout-button">
-              <button className="logout-btn">X</button>
+              <button className="logout-btn" onClick={handleClickLogout}>
+                X
+              </button>
             </div>
             <div className="account-button">
               <button className="account-btn">
